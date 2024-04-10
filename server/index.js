b8370ad2254b08803a35b0c41095fffe9dc1c75b
reza <sobhan014@gmail.com>
2024-04-09 21:57:58 -0600

    Adds MongoDB connection and sample code for inserting/finding documents

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const port = 3001;
@@ -16,3 +17,35 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+const mongodbUri = 'mongodb://localhost:27017';
+const client = new MongoClient(mongodbUri);
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Establish and verify connection
+  await client.db('dbname').command({ ping: 1 });
+  console.log('Connected successfully to mongodb server');
+
+  // Create a collection
+  await client.db('dbname').createCollection('sampleCollection');
+  console.log('Sample collection created');
+
+  // Insert a sample document in the collection
+  await client.db('dbname')
+    .collection('sampleCollection')
+    .insertOne({ name: 'name', email: 'email' });
+  console.log('1 document inserted');
+
+  // Find a document
+  const result = await client.db('dbname')
+    .collection('sampleCollection')
+    .findOne({ name: 'name' });
+  console.log('Found 1 document. email: ', result.email);
+} catch (e) {
+  console.error(e);
+} finally {
+  // Ensures that the client will close when you finish/error
+  await client.close();
+}
